afcc0c22eb8654a36aa35e3cb0e7d4b6f0dc6d74
David Merrick <david@david-merrick.com>
2019-12-10 17:51:30 -0800

    Forced to use env vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/davidmerrick/github-packages-demo")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
